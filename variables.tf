301f6383828767b9e0dbb29400736822ecf8ae0d
dk-il <165323380+dk-il@users.noreply.github.com>
2024-10-08 13:50:11 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "aws_region" {
   type    = string
-  default = "eu-central-1"
+  default = "us-east-2"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for main"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.13.13.0/24"
 }
 
 variable "availability_zones" {
   type    = string
-  default = "eu-central-1a"
+  default = "us-east-2a"
 }
